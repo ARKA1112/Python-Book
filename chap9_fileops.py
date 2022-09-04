c4a39e8abcda025e5d98c91167824b5a04de0185
= <=>
2022-09-04 18:49:52 +0530

    first gitlab commit

---
+++ chap9_fileops.py
@@ -0,0 +1,8 @@
+myfile = open('myfile.txt', 'w')      #Open for text output:create/empty
+myfile.write('hello text file\n')
+myfile.write('goodby text file\n')
+myfile.close()
+
+myfile = open('myfile.txt')    #opens file
+myfile.readline()              #reads line by line
+myfile.readline()
