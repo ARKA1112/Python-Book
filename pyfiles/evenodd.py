9278ea9fa1d45071cefb8b60ee77a875c6ebbe38
= <=>
2022-09-12 13:31:43 +0530

    few changes

---
+++ pyfiles/evenodd.py
@@ -0,0 +1,22 @@
+"""
+self made programme to check
+and print all  the numbers
+from the input to the 100
+and print the even ones
+"""
+#Docstrings should always be defined in double quotes
+self = input("enter a number ")
+try:
+    int(self)
+except:
+    print("input a number")
+self = int(self)
+cont = []
+while self < 100:
+    if self % 2 == 0:
+        print(self)
+        cont.append(self)
+        self +=1
+    else:
+        self+=1
+print("And contains total {} elements".format(len(cont)))
