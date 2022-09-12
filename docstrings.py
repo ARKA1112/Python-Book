0ab670e89709c8ef9b3ea3e40905831c719a939b
= <=>
2022-09-12 13:30:45 +0530

    evenodd.py created

--- docstrings.py
+++ docstrings.py
@@ -3,13 +3,15 @@ Module documentation
 Words Go Here
 """
 
-spam =40
+spam = 40
+
+
 def square(x):
     """
     function documentation
     can we have your liveer then?
     """
-    return x**2 # square
+    return x**2   # square
 class Employee:
     "class documentation"
     pass
