8e030d75a860ec96146864b362660a70fbec7adb
= <=>
2022-09-27 21:09:58 +0530

    .

--- person.py
+++ person.py
@@ -1,8 +1,20 @@
 class Person:
     def __init__(self,name,job=None,pay=0):                        #in operator overloading terms self is the newly created instance object
         self.name = name                                    #name job and pay become state information
-        self.job = job                                      #They are local variable within __init__ and when we do self.job it assigns an attribute named job referenced to the local variable name job although they name same but they are two different variables
+        self.job = job                                      #They are local variable within __init__ and when we do self.job it assigns an attribute named job                                                  referenced to the local variable name job although they name same but they are two different variables
         self.pay = pay
+    def giveRaise(self,percent):
+        self.pay = self.pay*(1+ percent/100)
+        return self.pay
+
+
+
+
+class Manager(Person):
+    def giveRaise(self, percent, bonus = 10):
+        Person.giveRaise(self, percent+bonus)
+    def __str__(self):
+        return '(Manager: %s %s %f' %(self.name, self.job,self.pay)
 
 
 if __name__ == '__main__':
@@ -14,3 +26,5 @@ if __name__ == '__main__':
     sue.pay *= 1.10
     print('%.2f'%(sue.pay))
     print(sue)
+    tom = Manager('Tom Jones','mgr',10000)
+    tom.giveRaise(10)
